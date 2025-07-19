d590f7a22ef35bd44fb6fdfe88318b54d8dab72c
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-07-19 22:30:34 +0200

    Center text in menu

--- src/dogfight2025.zig
+++ src/dogfight2025.zig
@@ -4,8 +4,8 @@ const rl = @cImport({
     @cInclude("raylib.h");
 });
 
-const window_width: u32 = 960;
-const window_height: u32 = 540;
+const window_width: u16 = 960;
+const window_height: u16 = 540;
 
 const screen = @import("Screen.zig");
 
@@ -44,7 +44,7 @@ pub fn run() !void {
         if (rl.IsKeyPressed(rl.KEY_SPACE)) {
             const result = screen.updateScreen(currentScreen, screen.Msg{ .inputClicked = screen.Inputs.GeneralAction });
             currentScreen = result.screen;
-            if(result.sideEffects.sound != null) {
+            if (result.sideEffects.sound != null) {
                 rl.PlaySound(boomSound);
             }
         }
@@ -53,10 +53,18 @@ pub fn run() !void {
     }
 }
 
+fn centerText(text: []const u8, y: u16, fontSize: u16) void {
+    const textWidth: u16 = @intCast(rl.MeasureText(text.ptr, fontSize));
+    const xPos: u16 = (window_width - textWidth) / 2;
+    rl.DrawText(text.ptr, xPos, y, fontSize, rl.LIGHTGRAY);
+}
+
+
 fn drawMenu(_: screen.MenuScreen) void {
     rl.ClearBackground(rl.RAYWHITE);
-    rl.DrawText("DogFight 2025", 200, 180, 20, rl.LIGHTGRAY);
-    rl.DrawText("Press SPACE to START!", 200, 220, 20, rl.LIGHTGRAY);
+    const textSize = 40;
+    centerText("Dogfight 2025", 180, textSize);
+    centerText("Press SPACE to START!", 220, 20);
 }
 
 fn drawGame(_: screen.GameScreen, planeTexture: rl.Texture2D, boomSound: rl.Sound) void {
