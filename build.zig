a9ca6a8b6b94b776721e42528ccb8b90d18ecf65
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-07-20 22:44:04 +0200

    Enable cross-compilation by switching to raylib master commit dependency (working build.zig file) and updating build.zig and .zig.zon appropriately

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const raySdk = @import("raylib");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -21,6 +22,7 @@ pub fn build(b: *std.Build) void {
         .linux_display_backend = .X11,
     });
     const raylib = raylib_dep.artifact("raylib");
+    raylib.addIncludePath(raylib_dep.path("src"));
 
     // We will also create a module for our other entry point, 'main.zig'.
     const exe_mod = b.createModule(.{
@@ -36,7 +38,7 @@ pub fn build(b: *std.Build) void {
     // This creates another `std.Build.Step.Compile`, but this one builds an executable
     // rather than a static library.
     const exe = b.addExecutable(.{
-        .name = "zig1",
+        .name = "DogFight2025",
         .root_module = exe_mod,
     });
     exe.linkLibrary(raylib);
