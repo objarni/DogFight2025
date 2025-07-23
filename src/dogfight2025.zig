cb61e6b8c91297d8acbc7df6f1fa3c4e146ec94a
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-07-23 20:07:41 +0200

    Set HDI window flag to avoid retina issue when windowed at least, and dump fb+win dimensions on boot

--- src/dogfight2025.zig
+++ src/dogfight2025.zig
@@ -14,31 +14,46 @@ test {
 }
 
 pub fn run() !void {
+    rl.SetConfigFlags(rl.FLAG_WINDOW_HIGHDPI);
     rl.InitWindow(window_width, window_height, "DogFight 2025");
     defer rl.CloseWindow();
     rl.InitAudioDevice();
     // rl.ToggleFullscreen();
 
+    const screen_w = rl.GetScreenWidth();
+    const screen_h = rl.GetScreenHeight();
+    const fb_w = rl.GetRenderWidth();
+    const fb_h = rl.GetRenderHeight();
+    std.debug.print("Window: {d}x{d}, Framebuffer: {d}x{d}\n", .{
+        screen_w, screen_h, fb_w, fb_h,
+    });
+
     const boomSound = rl.LoadSound("assets/boom.wav");
     defer rl.UnloadSound(boomSound);
 
     const planeTex = rl.LoadTexture("assets/plane.png");
+    if (!rl.IsTextureValid(planeTex)) {
+        std.debug.print("Texture failed!\n", .{});
+    }
     defer rl.UnloadTexture(planeTex);
 
     const cloudTex = rl.LoadTexture("assets/CloudBig.png");
+    if (!rl.IsTextureValid(cloudTex)) {
+        std.debug.print("Texture failed!\n", .{});
+    }
     defer rl.UnloadTexture(cloudTex);
 
     var currentScreen = screen.Screen.init();
     var drawAverage: i128 = 0;
     var drawAverageCount: u32 = 0;
 
-    const ally : std.mem.Allocator = std.heap.page_allocator;
+    const ally: std.mem.Allocator = std.heap.page_allocator;
 
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
 
         // Draw
-        const before : i128 = std.time.nanoTimestamp();
+        const before: i128 = std.time.nanoTimestamp();
         switch (currentScreen) {
             .menu => |_| {
                 drawMenu(currentScreen.menu);
@@ -47,10 +62,10 @@ pub fn run() !void {
                 drawGame(currentScreen.game, planeTex, cloudTex);
             },
         }
-        const after : i128 = std.time.nanoTimestamp();
+        const after: i128 = std.time.nanoTimestamp();
         drawAverage += after - before;
         drawAverageCount += 1;
-        if(drawAverageCount == 10000) {
+        if (drawAverageCount == 10000) {
             const average: i128 = @divTrunc(@divTrunc(drawAverage, drawAverageCount), 1000);
             std.debug.print("average draw time: {d} ms\n", .{average});
             drawAverage = 0;
@@ -99,7 +114,7 @@ fn centerText(text: []const u8, y: u16, fontSize: u16, color: rl.Color) void {
 }
 
 fn drawMenu(menu: screen.MenuState) void {
-    rl.ClearBackground(rl.BLACK);
+    rl.ClearBackground(rl.YELLOW);
     const textSize = 40;
     centerText("Dogfight 2025", 180, textSize, rl.GREEN);
     if (menu.blink)
@@ -108,6 +123,8 @@ fn drawMenu(menu: screen.MenuState) void {
 
 fn drawGame(state: screen.GameState, planeTex: rl.Texture2D, cloudTex: rl.Texture2D) void {
     rl.ClearBackground(rl.RAYWHITE);
+
+    rl.DrawCircle(200, 200, 50, rl.RED);
 
     rl.DrawTexture(planeTex, 50, 50, rl.WHITE);
     rl.DrawTexture(planeTex, 150, 50, rl.GREEN);
