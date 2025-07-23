6c510d0251959570eee63623a3b014632261867a
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-07-23 21:06:29 +0200

    Implement rudimentary velocity+acceleration

--- src/Plane.zig
+++ src/Plane.zig
@@ -8,6 +8,7 @@ const v = v2.v;
 const PlaneConstants = struct {
     initialPos : V,
     takeoffSpeed: f32,
+    groundAccelerationPerS: f32,
 };
 
 const PlaneState = enum {
@@ -21,12 +22,14 @@ const Plane = struct {
     position: V,
     velocity: V,
     state: PlaneState,
+    planeConstants: PlaneConstants,
 
     pub fn init(constants: PlaneConstants) Plane {
         return Plane{
             .position = constants.initialPos,
             .velocity = v(0, 0),
             .state = .STILL,
+            .planeConstants = constants,
         };
     }
 
@@ -36,6 +39,7 @@ const Plane = struct {
                 .state = .TAKEOFF_ROLL,
                 .position = self.position,
                 .velocity = self.velocity,
+                .planeConstants = self.planeConstants,
             };
             return newState;
         }
@@ -48,8 +52,23 @@ const Plane = struct {
                 .state = .TAKEOFF_ROLL,
                 .position = self.position,
                 .velocity = self.velocity,
+                .planeConstants = self.planeConstants,
             };
             return newState;
+        }
+        return self;
+    }
+
+    pub fn timePassed(self: Plane, seconds: f32) Plane {
+        if (self.state == .TAKEOFF_ROLL) {
+            const newVelocity = self.velocity + v(self.planeConstants.groundAccelerationPerS * seconds, 0);
+            const newPosition = self.position + v(newVelocity[0] * seconds, 0);
+            return Plane{
+                .position = newPosition,
+                .velocity = newVelocity,
+                .state = self.state,
+                .planeConstants = self.planeConstants,
+            };
         }
         return self;
     }
@@ -58,6 +77,7 @@ const Plane = struct {
 const testPlaneConstants = PlaneConstants{
     .initialPos = v(50, 200),
     .takeoffSpeed = 50.0,
+    .groundAccelerationPerS = 10.0,
 };
 
 test {
@@ -66,22 +86,27 @@ test {
         .position = v(50, 200),
         .velocity = v(0, 0),
         .state = .STILL,
+        .planeConstants = testPlaneConstants
     };
     try std.testing.expectEqual(expected, plane);
 }
 
 test {
     const plane = Plane.init(testPlaneConstants);
-    // Act
     const newPlane = plane.rise();
     try std.testing.expectEqual(PlaneState.TAKEOFF_ROLL, newPlane.state);
 }
 
 test {
     const plane = Plane.init(testPlaneConstants);
-    // Act
     const newPlane = plane.dive();
     try std.testing.expectEqual(PlaneState.TAKEOFF_ROLL, newPlane.state);
+}
+
+test "plane acceleration on ground during takeoff roll" {
+    const plane = Plane.init(testPlaneConstants);
+    const newPlane = plane.rise().timePassed(1.0);
+    try std.testing.expectEqual(10.0, newPlane.velocity[0]);
 }
 
 // #plane initial state is STILL
