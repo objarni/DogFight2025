5bc0f9254f1b11c196da971d5145f8b176549017
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-08-23 10:28:33 +0200

    Update TODO

--- src/dogfight/dogfight2025.zig
+++ src/dogfight/dogfight2025.zig
@@ -479,4 +479,3 @@ test {
 // TODO: figure out how to play menu theme at start of game (some kind of init for states?)
 // TODO: refactor away from array_list.Managed to std.ArrayList
 // TODO: Switch explosion array to array_list.Managed
-// TODO: shadows beneath planes
